2b485babacb36a2298f4fd67bd31b1cf1ce79415
P-XIE <129963950+P-XIE@users.noreply.github.com>
2023-08-10 17:23:12 +0800

    Create utils.py

---
+++ reverie/backend_server/utils.py
@@ -0,0 +1,15 @@
+openai_api_key = "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"
+# Put your name
+key_owner = "XIE"
+
+maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+env_matrix = f"{maze_assets_loc}/the_ville/matrix"
+env_visuals = f"{maze_assets_loc}/the_ville/visuals"
+
+fs_storage = "../../environment/frontend_server/storage"
+fs_temp_storage = "../../environment/frontend_server/temp_storage"
+
+collision_block_id = "32125"
+
+# Verbose 
+debug = True
